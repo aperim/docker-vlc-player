56fde6dd4d47934abc1e93046db1e78fdd4f3a74
Troy Kelly <troy@aperim.com>
2021-07-18 16:50:54 +1000

    Fixing start

--- source/usr/local/sbin/videogo.sh
+++ source/usr/local/sbin/videogo.sh
@@ -1,4 +1,53 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+VLC=$(command -v cvlc)
+
+if [ -z "${VLC_SOURCE_URL}" ]; then
+    echo "Source URL not defined (VLC_SOURCE_URL)"
+    exit 1
+fi
+
+if [ -z "${VLC_BITRATE}" ]; then
+    VLC_BITRATE=1024
+fi
+
+if [ -z "${VLC_CACHE}" ]; then
+    VLC_CACHE=1024
+fi
+
+if [ -z "${VLC_THREADS}" ]; then
+    VLC_THREADS=$(nproc --all)
+fi
+
+if [ -z "${VLC_ASPECT_RATIO}" ]; then
+    VLC_ASPECT_RATIO=16:9
+fi
+
+if [ -z "${VLC_ADAPTIVE_WIDTH}" ]; then
+    VLC_ADAPTIVE_WIDTH=1280
+fi
+
+if [ -z "${VLC_ADAPTIVE_HEIGHT}" ]; then
+    VLC_ADAPTIVE_HEIGHT=720
+fi
+
+if [ -z "${VLC_ADAPTIVE_BITRATE}" ]; then
+    VLC_ADAPTIVE_BITRATE=2048
+fi
+
+if [ -z "${VLC_ADAPTIVE_LOGIC}" ]; then
+    VLC_ADAPTIVE_LOGIC=highest
+fi
+
+if [ -z "${VLC_VERBOSE}" ]; then
+    VLC_VERBOSE=0
+fi
+
+if [ -z "${VLC_ZOOM}" ]; then
+    VLC_ZOOM=1
+fi
+
+VLC_AVCODEC_OPTIONS="--avcodec-dr --avcodec-corrupted --avcodec-hurry-up --avcodec-skip-frame=1 --avcodec-skip-idct=1 --avcodec-fast --avcodec-threads=${VLC_THREADS} --sout-avcodec-strict=-2"
 
 # Allow VLC to run as root
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
@@ -27,4 +76,5 @@ sleep 5
 unclutter -display ${DISPLAY} -idle 0.1 &
 
 # Start the VLC media player
-/usr/bin/cvlc -q --no-osd -L -f --no-video-title-show --x11-display ${DISPLAY} rtp://@234.0.1.255:1234
+# /usr/bin/cvlc -q --no-osd -L -f --no-video-title-show --x11-display ${DISPLAY} --zoom 2 --no-repeat --no-loop --drop-late-frames --skip-frames --play-and-exit rtp://@234.0.1.255:1234 vlc://quit
+${VLC} -I telnet --verbose=${VLC_VERBOSE} --no-disable-screensaver ${VLC_AVCODEC_OPTIONS} --zoom ${VLC_ZOOM} --no-repeat --no-loop --network-caching=${VLC_CACHE} --drop-late-frames --skip-frames --play-and-exit --no-daemon --adaptive-logic="${VLC_ADAPTIVE_LOGIC}" --adaptive-maxwidth=${VLC_ADAPTIVE_WIDTH} --adaptive-maxheight=${VLC_ADAPTIVE_HEIGHT} --adaptive-bw=${VLC_ADAPTIVE_BITRATE} "${VLC_SOURCE_URL}" vlc://quit
